3cb9c0c5de52bb17ef6807f9c0355e532883d0c5
lst68868 <65935106+lst68868@users.noreply.github.com>
2023-05-11 12:41:30 -0400

    add descriptive comments for js

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+// Get references to HTML elements
 const dealBtn = document.getElementById("deal-btn");
 const hitBtn = document.getElementById("hit-btn");
 const standBtn = document.getElementById("stand-btn");
 const message = document.getElementById("message");
-
 const playerCards = document.getElementById("player-cards");
 const dealerCards = document.getElementById("dealer-cards");
 const playerScore = document.getElementById("player-score");
 const dealerScore = document.getElementById("dealer-score");
 
+// Define arrays for suits and values of cards
 const suits = ["C", "D", "H", "S"];
 const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+// Keep track of played cards
 let playedCards = [];
 
+// Initialize player and dealer hands, and count
 let playerHand = [];
 let dealerHand = [];
 let count = 0;
+
+// Display initial count on the page
 document.getElementById("count").innerText = `The count is...`;
 
+// Event listener for the "Deal" button
 dealBtn.addEventListener("click", () => {
     message.textContent = ''; // Clear the win/loss message
     dealBtn.disabled = true;
@@ -26,6 +33,7 @@ dealBtn.addEventListener("click", () => {
     dealHand();
 });
 
+// Event listener for the "Hit" button
 hitBtn.addEventListener("click", () => {
     playerHand.push(getRandomCard());
     displayCards(playerHand, playerCards);
@@ -33,21 +41,24 @@ hitBtn.addEventListener("click", () => {
     checkBust(playerHand, "player");
 });
 
+// Event listener for the "Stand" button
 standBtn.addEventListener("click", () => {
     document.querySelector("h3").innerText = "Click 'deal' to play the next hand.";
     dealerTurn();
 });
 
+// Function to get a random card from the deck
 function getRandomCard() {
     const suit = suits[Math.floor(Math.random() * suits.length)];
     const value = values[Math.floor(Math.random() * values.length)];
 
-    
+    // Check if the card has already been played
     if (playedCards.includes(`${value}${suit}`)) {
         return getRandomCard();
     }
 
-    if(playedCards.length === 52) {
+    // Check if all cards have been played, then reset the game
+    if (playedCards.length === 52) {
         resetGame();
     }
 
@@ -55,27 +66,28 @@ function getRandomCard() {
     return { value, suit };
 }
 
+// Function to get the count value of a card for card counting purposes
 function getCardCountValue(card) {
     if (["A", "10", "J", "Q", "K"].includes(card.value)) {
-      return -1;
+        return -1;
     } else if (["2", "3", "4", "5", "6"].includes(card.value)) {
-      return 1;
+        return 1;
     } else {
-      return 0;
+        return 0;
     }
-  }
-  
-  function incrementCount() {
+}
+
+// Function to increment the count based on the player and dealer hands
+function incrementCount() {
     for (let i = 0; i < playerHand.length; i++) {
-      count += getCardCountValue(playerHand[i]);
+        count += getCardCountValue(playerHand[i]);
     }
     for (let i = 0; i < dealerHand.length; i++) {
-      count += getCardCountValue(dealerHand[i]);
+        count += getCardCountValue(dealerHand[i]);
     }
-  
+
     document.getElementById("count").innerText = `The count is...${count}.`;
-  }
-  
+}
 
 function getCardValue(card) {
     if (card.value === "A") {
@@ -185,7 +197,7 @@ function dealerTurn() {
     endGame();
 }
 
-
+// Function to reset the game state
 function resetGame() {
     playerHand = [];
     dealerHand = [];
@@ -198,11 +210,14 @@ function resetGame() {
     displayScore(dealerHand, dealerScore);
 }
 
+// Function to end the current game round
 function endGame() {
     dealBtn.disabled = false;
     hitBtn.disabled = true;
     standBtn.disabled = true;
 }
+
+//Code stored for later use:
 
 // <div class="icon-top">
                 //     <img class="icon" src="card-back.png"></img>
